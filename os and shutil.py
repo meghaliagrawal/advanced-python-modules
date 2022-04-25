b3710ec55ed17c8cf46afcfdf85211f1f2433b06
mishka <meghali2892@gmail.com>
2022-04-25 11:31:56 +0530

    add: os nd shutil functions

---
+++ os and shutil.py
@@ -0,0 +1,9 @@
+import os
+code =1
+if code == 1:
+	#reading and writing a text file 
+	f = open("practice.txt","w+")
+	f.write("i have created a nex text file named practice.txt")
+	print (f"my txt file has been created at the location: {os.getcwd()}")
+	f.close()
+	
