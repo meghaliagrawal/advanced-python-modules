a21a47064fdc9484b53ff39d082d648029898e83
meghaliagrawal <98209492+meghaliagrawal@users.noreply.github.com>
2022-04-25 11:44:05 +0530

    Update os and shutil.py
    added print new

--- os and shutil.py
+++ os and shutil.py
@@ -5,5 +5,6 @@ if code == 1:
 	f = open("practice.txt","w+")
 	f.write("i have created a nex text file named practice.txt")
 	print (f"my txt file has been created at the location: {os.getcwd()}")
+	print ("abcd")
 	f.close()
 	
